fcbb71ca7e798d925e674e1e3212935d0770bba2
Felipe Herrera (Fhilip) <felsul@hotmail.com>
2019-06-06 13:14:46 -0500

    Update itcontent.php

--- lang/en/itcontent.php
+++ lang/en/itcontent.php
@@ -6,18 +6,18 @@
    * @category   Components
    * @package    Moodle
    * @subpackage Mod_Itcontent
-   * @author     JFHR <felipe.herrera@iteraprocess.com>
+   * @author     JFHR <felsul@hotmail.com>
    * @license    https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
-   * @link       https://aulavirtual.issste.gob.mx
+   * @link      
    */
 defined('MOODLE_INTERNAL') || die();
 
-$string['modulename'] = 'Content ISSSTE';
-$string['modulenameplural'] = 'Content ISSSTE';
+$string['modulename'] = 'Content ';
+$string['modulenameplural'] = 'Content ';
 $string['parentorg'] = 'Parent';
 $string['itcontent_help'] = 'This is the content of the help tooltip.';
-$string['itcontent'] = 'Content ISSSTE';
+$string['itcontent'] = 'Content ';
 $string['pluginadministration'] = 'Content ISSSTE Administration';
-$string['pluginname'] = 'Content Aula ISSSTE Plugin ';
+$string['pluginname'] = 'Content Aula  Plugin ';
 $string['requireloginerror'] = 'Content not accessible. Require Login';
 $string['requireidcontenterror'] = 'Content Secction. Require Id of Content';
