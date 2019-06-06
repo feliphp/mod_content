b516518ed4b86c89eb5360994fb23ad7272b0200
Felipe Herrera (Fhilip) <felsul@hotmail.com>
2019-06-06 13:15:23 -0500

    Update itcontent.php

--- lang/es_mx/itcontent.php
+++ lang/es_mx/itcontent.php
@@ -6,18 +6,18 @@
    * @category   Components
    * @package    Moodle
    * @subpackage Mod_Itcontent
-   * @author     JFHR <felipe.herrera@iteraprocess.com>
+   * @author     JFHR <felsul@hotmail.com>
    * @license    https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
-   * @link       https://aulavirtual.issste.gob.mx
+   * @link       
    */
 defined('MOODLE_INTERNAL') || die();
 
-$string['modulename'] = 'Contenidos Aula Virtual ISSSTE';
-$string['modulenameplural'] = 'Contenidos Aula Virtual ISSSTE';
+$string['modulename'] = 'Contenidos Aula Virtual ';
+$string['modulenameplural'] = 'Contenidos Aula Virtual ';
 $string['parentorg'] = 'Parent';
 $string['itcontent_help'] = 'Este es el contenido de la ayuda.';
-$string['itcontent'] = 'Contenidos Aula Virtual ISSSTE';
+$string['itcontent'] = 'Contenidos Aula Virtual ';
 $string['pluginadministration'] = 'Contenidos Aula Administración';
-$string['pluginname'] = 'Contenidos Aula Virtual ISSSTE Plugin ';
+$string['pluginname'] = 'Contenidos Aula Virtual  Plugin ';
 $string['requireloginerror'] = 'Módulo no accesible. Requiere Login';
 $string['requireidcontenterror'] = 'Sección de Contenido. Sin Id de Contenido';
